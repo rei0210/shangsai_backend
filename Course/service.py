8d479b29a5cdec07a5250b5e9e7ebe81148cff5a
bianjiangpan <2545878690@qq.com>
2025-03-03 22:02:46 -0500

    course数据库操作函数

--- Course/service.py
+++ Course/service.py
@@ -10,11 +10,13 @@ def get_all_courses():
     return Course.manager.get_all()
 
 def get_course_by_id(course_id):
-    return Course.manager.get_course(course_id)
+    return Course.manager.get_course_by_id(course_id)
+
 def get_course_by_name(course_name):
-    return Course.manager.get_course(course_name)
-def get_course_by_simplified_name(course_name):
-    return Course.manager.get_course(course_name, simplified_name=course_name)
+    return Course.manager.get_course_by_name(course_name)
+
+def get_course_by_simplified_name(simplified_name):
+    return Course.manager.get_course_by_simplified_name(simplified_name)
 
 def delete_course_by_id(course_id):
     return Course.manager.delete_course(course_id)
